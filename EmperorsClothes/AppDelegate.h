e74c1928af3f5a4476eb17794e252e65148dcc0e
Sevy11 <michaelsevy@gmail.com>
2015-05-23 09:29:28 -0500

    Initial Commit

---
+++ EmperorsClothes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmperorsClothes
+//
+//  Created by Michael Sevy on 5/23/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmperorsClothes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmperorsClothes
+//
+//  Created by Michael Sevy on 5/23/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
